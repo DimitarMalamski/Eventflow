c46b5d62836b3e892b77df054a5788864ad628c9
DimitarMalamski <mitemalamata@gmail.com>
2025-02-26 19:30:13 +0100

    Creating the Authentication interface

---
+++ Services/Interfaces/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using Eventflow.Models.Models;
+
+namespace Eventflow.Services.Interfaces
+{
+    public interface IAuthService
+    {
+        bool RegisterUser(User user);
+        User? AuthenticateUser(string username, string password);
+    }
+}
---
+++ Utilities/PasswordHasher.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Eventflow.Utilities
+{
+    public static class PasswordHasher
+    {
+        public static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                byte[] hashByte = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(hashByte);
+            }
+        }
+        public static bool VerifyPassword(string password, string storedHash)
+        {
+            string hashedPassword = HashPassword(password);
+            return hashedPassword == storedHash;
+        }
+    }
+}
